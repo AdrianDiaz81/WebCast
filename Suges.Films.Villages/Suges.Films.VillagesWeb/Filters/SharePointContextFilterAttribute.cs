5fad0103a556156a7adb33b30eb7fea8b5f1078b
Adrián Díaz Cervera <adiaz@encamina.com>
2016-01-26 13:33:58 +0100

    Upload changes

--- Suges.Films.Villages/Suges.Films.VillagesWeb/Filters/SharePointContextFilterAttribute.cs
+++ Suges.Films.Villages/Suges.Films.VillagesWeb/Filters/SharePointContextFilterAttribute.cs
@@ -8,25 +8,25 @@ namespace Suges.Films.VillagesWeb
     /// </summary>
     public class SharePointContextFilterAttribute : ActionFilterAttribute
     {
-        //public override void OnActionExecuting(ActionExecutingContext filterContext)
-        //{
-        //    if (filterContext == null)
-        //    {
-        //        throw new ArgumentNullException("filterContext");
-        //    }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
 
-        //    Uri redirectUrl;
-        //    switch (SharePointContextProvider.CheckRedirectionStatus(filterContext.HttpContext, out redirectUrl))
-        //    {
-        //        case RedirectionStatus.Ok:
-        //            return;
-        //        case RedirectionStatus.ShouldRedirect:
-        //            filterContext.Result = new RedirectResult(redirectUrl.AbsoluteUri);
-        //            break;
-        //        case RedirectionStatus.CanNotRedirect:
-        //            filterContext.Result = new ViewResult { ViewName = "Error" };
-        //            break;
-        //    }
-        //}
+            Uri redirectUrl;
+            switch (SharePointContextProvider.CheckRedirectionStatus(filterContext.HttpContext, out redirectUrl))
+            {
+                case RedirectionStatus.Ok:
+                    return;
+                case RedirectionStatus.ShouldRedirect:
+                    filterContext.Result = new RedirectResult(redirectUrl.AbsoluteUri);
+                    break;
+                case RedirectionStatus.CanNotRedirect:
+                    filterContext.Result = new ViewResult { ViewName = "Error" };
+                    break;
+            }
+        }
     }
 }
